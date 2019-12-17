c3e63d0288a98fc9a7a5f606eb2e7b85b84cbf4d
Steven chiu <b03901045@ntu.edu.tw>
2019-12-17 15:23:39 +0800

    create image

--- MODEL_PerGoldbeter1996.jl
+++ MODEL_PerGoldbeter1996.jl
@@ -2,6 +2,7 @@ using ModelingToolkit
 using DifferentialEquations
 using Plots
 
+#PARAMETERS
 @parameters t
 @parameters vs vm vd
 @parameters ks k1 k2
@@ -10,6 +11,7 @@ using Plots
 @variables m(t) p0(t) p1(t) p2(t) pn(t)
 @derivatives D'~t
 
+#CHEMICAL REACTIONS
 mm(v, k, sub) = v*sub/(k+sub)
 hill(v,p,k,n) = v/(1+(p/k)^n)
 
@@ -22,26 +24,30 @@ eqs = [
     D(pn) ~ k1*p2-k2*pn
 ]
 
+#DEFINE PROBLEM
 de = ODESystem(eqs)
 PER = ODEFunction(de,
                 [m,p0, p1, p2, pn],
                 [vs,vm,vd,ks,k1,k2,V1,V2,V3,V4,K1,K2,K3,K4,KI,Km1,Kd,n]
                 )
 
+#SET VALUE
 u0 = [2,0,2.0,2.0,2.0]
-
-
-p = ( 
+p = (
     vs=0.76, vm=0.65, vd=0.95,
     ks=0.38, k1=1.9, k2=1.3,
     V1=3.2, V2=1.58, V3=5.0, V4=2.5,
     K1=1.0, K2=1.0, K3=1.0, K4=1.0,
     KI=1.0, Km1=0.5, Kd=0.2, n=4.0
     )
-
 tspan = (0.0, 100.0)
 
+#SOLVE
 prob = ODEProblem(PER, u0, tspan, values(p))
 sol = solve(prob)
 
-plot(sol, vars=(1,2,3), lw=1)
+#PLOTTING
+labels = ["m" "p0" "p1" "p2" "pn"]
+plot(sol, vars=(1,2,3), label=labels[1:3])
+
+plot(sol, label=labels)
